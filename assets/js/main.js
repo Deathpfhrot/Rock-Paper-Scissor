889dfe565f756ba017dbdf5cb5ffb3052a0f2036
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-25 21:09:21 +0100

    rock paper scissor

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//JS Deep Projekt lev3_9: Rock, Paper, Scissors
+
+// Definieren wieviele Runden wir gegen den Comput spielen
+// Die Farbe sollte sich ändern, wenn es sic um einen Gewinn, ein Unentschieden oder eine Niederlage handelt.
+//Das Design im Anhang ist nur ein Vorschlag, ihr könnt hier gerne kreativ werden.

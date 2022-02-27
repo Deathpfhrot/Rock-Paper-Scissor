f1b0136f6ecb9d78c1bcb8f8a52c9edaab240fa1
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-27 22:44:05 +0100

    added color change

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ let amountOfTries = undefined;
 let roundRemoves = document.getElementById('roundRemoves')
 let roundRemoves2 = document.getElementById('roundRemoves2')
 let headline = document.getElementById('headline')
-let guessCount = 1;
+let guessCount = 0;
 
 
 setTries = (amount) => {
@@ -62,55 +62,145 @@ setTries = (amount) => {
     roundRemoves2.classList.add('invisible')
     roundRemoves2.classList.remove('container-rounds')
     printGuessTries(guessCount);
+
 }
 
 printGuessTries = (number) => {
     headline.innerHTML = `
     ${number} / ${amountOfTries}`
+
 }
+
+let divScissor = document.querySelector('#divScissor')
+let divPaper = document.querySelector('#divPaper')
+let divRock = document.querySelector('#divRock')
+let divScissorCopy = document.querySelector('#divScissor')
+let divPaperCopy = document.querySelector('#divPaper')
+let divRockCopy = document.querySelector('#divRock')
 
 function getResult() {
 
-
     guessCount++
+
+
 
     if (gameOver()) {
         return;
     }
 
+
     printGuessTries(guessCount)
+
+
 
     if (computerChoice === 'rock' && userChoice === 'paper') {
         resultDisplay.innerHTML = ' You won!'
         counterPO++
         playerOne.innerHTML = counterPO
+        divScissor.style.removeProperty('border')
+        divPaper.style.border = '3px solid green'
+        divPaper.style.transform = '1s'
+        divPaper.style.transition = '1s ease-in-out'
+        divRock.style.removeProperty('border')
+
     } else if (computerChoice === 'scissors' && userChoice === 'paper') {
         resultDisplay.innerHTML = ' You lose!'
         counterPC++
         playerComputer.innerHTML = counterPC
+        divScissor.style.removeProperty('border')
+            // divPaper.style.removeProperty('border')
+        divRock.style.removeProperty('border')
+        divPaper.style.border = '3px solid red'
+        divPaper.style.transform = '1s'
+        divPaper.style.transition = '1s ease-in-out'
+
+
     } else if (computerChoice === 'paper' && userChoice === 'rock') {
         resultDisplay.innerHTML = ' You lose!'
         counterPC++
         playerComputer.innerHTML = counterPC
+        divScissor.style.removeProperty('border')
+        divPaper.style.removeProperty('border')
+            // divRock.style.removeProperty('border')
+        divRock.style.border = '3px solid red'
+        divRock.style.transform = '1s'
+        divRock.style.transition = '1s ease-in-out'
+
+
     } else if (computerChoice === 'scissors' && userChoice === 'rock') {
         resultDisplay.innerHTML = ' You won!'
         counterPO++
         playerOne.innerHTML = counterPO
+        divScissor.style.removeProperty('border')
+        divPaper.style.removeProperty('border')
+        divRock.style.border = '3px solid green'
+        divRock.style.transform = '1s'
+        divRock.style.transition = '1s ease-in-out'
+
     } else if (computerChoice === 'rock' && userChoice === 'scissors') {
         resultDisplay.innerHTML = ' You lose!'
         counterPC++
         playerComputer.innerHTML = counterPC
+            // divScissor.style.removeProperty('border')
+        divPaper.style.removeProperty('border')
+        divRock.style.removeProperty('border')
+        divScissor.style.border = '3px solid red'
+        divScissor.style.transform = '1s'
+        divScissor.style.transition = '1s ease-in-out'
+
+
     } else if (computerChoice === 'paper' && userChoice === 'scissors') {
         resultDisplay.innerHTML = ' You won!'
         counterPO++
         playerOne.innerHTML = counterPO
-    } else {
+        divScissor.style.border = '3px solid green'
+        divScissor.style.transform = '1s'
+        divScissor.style.transition = '1s ease-in-out'
+        divPaper.style.removeProperty('border')
+        divRock.style.removeProperty('border')
+
+
+    } else if (computerChoice === 'paper' && userChoice === 'paper') {
+        divPaperCopy.style.border = '3px solid gray'
+        divPaperCopy.style.transform = '1s'
+        divPaperCopy.style.transition = '1s ease-in-out'
         resultDisplay.innerHTML = " It's a draw!"
-        counterPO++
-        counterPC++
-        playerOne.innerHTML = counterPO
-        playerComputer.innerHTML = counterPC
+        divRockCopy.style.removeProperty('border')
+        divScissorCopy.style.removeProperty('border')
+
+    } else if (computerChoice === 'rock' && userChoice === 'rock') {
+        divRockCopy.style.border = '3px solid gray'
+        divRockCopy.style.transform = '1s'
+        divRockCopy.style.transition = '1s ease-in-out'
+        resultDisplay.innerHTML = " It's a draw!"
+
+        divPaperCopy.style.removeProperty('border')
+        divScissorCopy.style.removeProperty('border')
+
+    } else if (computerChoice === 'scissors' && userChoice === 'scissors') {
+        divScissorCopy.style.border = '3px solid gray'
+        divScissorCopy.style.transform = '1s'
+        divScissorCopy.style.transition = '1s ease-in-out'
+        resultDisplay.innerHTML = " It's a draw!"
+        divRockCopy.style.removeProperty('border')
+        divPaperCopy.style.removeProperty('border')
+
+
+
+    } else {
+        console.log('hello');
+        // resultDisplay.innerHTML = " It's a draw!"
+        // counterPO++
+        // counterPC++
+        // playerOne.innerHTML = counterPO
+        // playerComputer.innerHTML = counterPC
+        // divScissor.style.removeProperty('border')
+        // divPaper.style.removeProperty('border')
+        // divRock.style.removeProperty('border')
+
     }
+
+
 }
 let finisher = document.getElementById('finisher')
 
